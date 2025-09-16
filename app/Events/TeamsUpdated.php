29de9efc22bc7e73a626c12b6946ae38cd0e815d
Ahairwe Jordan <2000809725@std.kyu.ac.ug>
2025-09-16 07:56:44 +0300

    attempt to add pusher

--- app/Events/TeamsUpdated.php
+++ app/Events/TeamsUpdated.php
@@ -10,16 +10,18 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class TeamsUpdated
+class TeamsUpdated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $tournament;
 
     /**
      * Create a new event instance.
      */
-    public function __construct()
+    public function __construct($tournament)
     {
-        //
+        $this->tournament = $tournament;
     }
 
     /**
@@ -30,7 +32,7 @@ class TeamsUpdated
     public function broadcastOn(): array
     {
         return [
-            new PrivateChannel('channel-name'),
+            new Channel('my-channel'),
         ];
     }
 }
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -9,6 +9,20 @@
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+{{-- {{ config('app.pusher.app_key') }} --}}
+{{-- add pusher js --}}
+<script src="https://js.pusher.com/8.2.0/pusher.min.js"></script>
+<script>
+
+var pusher = new Pusher('{{ config('app.pusher.app_key') }}', {
+    cluster: '{{ config('app.pusher.app_cluster') }}',
+});
+
+var channel = pusher.subscribe('my-channel');
+channel.bind('my-event', function(data) {
+    alert(JSON.stringify(data));
+});
+</script>
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
